b634ba323135b89be62db50ac00e86b762875bd6
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-08-16 23:27:31 +0200

    Add oplossing examen movies

---
+++ examen/Movies-Exam-oplossing/src/test/java/edu/ap/spring/test/SpringTest.java
@@ -0,0 +1,16 @@
+package edu.ap.spring.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+import org.junit.jupiter.api.*;
+
+@Configuration
+@SpringBootTest
+public class SpringTest {
+
+    @Test
+    public void testSearchovieByActor() {
+
+    }
+}
