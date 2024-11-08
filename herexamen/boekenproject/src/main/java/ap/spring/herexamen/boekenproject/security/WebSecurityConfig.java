0d1f84d50d02d2099e6f66856ed40751932e76b2
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-11-08 23:50:55 +0100

    Update WebSecurityConfig

--- herexamen/boekenproject/src/main/java/ap/spring/herexamen/boekenproject/security/WebSecurityConfig.java
+++ herexamen/boekenproject/src/main/java/ap/spring/herexamen/boekenproject/security/WebSecurityConfig.java
@@ -20,57 +20,62 @@ import java.util.ArrayList;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Autowired
-    private WebUserRepository repo;
+        @Autowired
+        private WebUserRepository repo;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/login", "/h2-console/**").permitAll()
-                        //.requestMatchers("/home").hasRole("WEBUSER") ------------------------ uncomment dit voor werkende authenticatie
-                        .anyRequest().authenticated())
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll())
-                .logout((logout) -> logout.permitAll());
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService users() {
-
-		/*UserDetails user = User.builder()
-                .username("user")
-                .password(ENCODED_PASSWORD)
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(ENCODED_PASSWORD)
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);*/
-
-        return new InMemoryUserDetailsManager(reloadUsers(repo));
-    }
-
-    public static ArrayList<UserDetails> reloadUsers(WebUserRepository repo) {
-        ArrayList<UserDetails> userDetails = new ArrayList<UserDetails>();
-        Iterable<WebUser> users = repo.findAll();
-        for (WebUser u : users) {
-            UserDetails user = User.builder()
-                    .username(u.getUsername())
-                    .password(u.getPassword())
-                    .build();
-            userDetails.add(user);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                /*
+                 * http
+                 * .authorizeHttpRequests((requests) -> requests
+                 * .requestMatchers("/login", "/h2-console/**").permitAll()
+                 * .requestMatchers("/home").hasRole("WEBUSER")
+                 * // uncomment dit voor werkende authenticatie
+                 * .anyRequest().authenticated())
+                 * 
+                 * .formLogin((form) -> form
+                 * .loginPage("/login")
+                 * .permitAll())
+                 * .logout((logout) -> logout.permitAll());
+                 */
+                return http.build();
         }
-        return userDetails;
-    }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public UserDetailsService users() {
+
+                /*
+                 * UserDetails user = User.builder()
+                 * .username("user")
+                 * .password(ENCODED_PASSWORD)
+                 * .roles("USER")
+                 * .build();
+                 * UserDetails admin = User.builder()
+                 * .username("admin")
+                 * .password(ENCODED_PASSWORD)
+                 * .roles("USER", "ADMIN")
+                 * .build();
+                 * return new InMemoryUserDetailsManager(user, admin);
+                 */
+
+                return new InMemoryUserDetailsManager(reloadUsers(repo));
+        }
+
+        public static ArrayList<UserDetails> reloadUsers(WebUserRepository repo) {
+                ArrayList<UserDetails> userDetails = new ArrayList<UserDetails>();
+                Iterable<WebUser> users = repo.findAll();
+                for (WebUser u : users) {
+                        UserDetails user = User.builder()
+                                        .username(u.getUsername())
+                                        .password(u.getPassword())
+                                        .build();
+                        userDetails.add(user);
+                }
+                return userDetails;
+        }
+
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
